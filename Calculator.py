b72d1db14ffc299bfd4fef1373efc7d7872e6e55
Sahatsachai1166109060796 <156157261+Asis1166109060796@users.noreply.github.com>
2024-03-03 21:05:08 +0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+def plus():#สหัสชัย สิทธิมงคล
+            a = float(input(": "))
+            b = float(input(": "))
+            ผลลัพธ์ = a + b
+            print(ผลลัพธ์)
+
+def minus():
+def multi():
+def divi():
+while True:
+    print("บวก กด 1")
+    print("ลบ กด 2")
+    print("คูณ กด 3")
+    print("หาร กด 4")
+
+    choose=int(input("กรอกวิธีคำนวณ"))
+    if (choose==1):
+        plus()
+    elif(choose==):
+        minus()
+    elif(choose==):
+        multi()
+    elif(choose==):
+        divi()
+    else:
+        print("error")
